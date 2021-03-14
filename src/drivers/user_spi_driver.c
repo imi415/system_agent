e9dcd5074813f5d7d15e07a18c0bc47c86345f03
imi415 <imi415.public@gmail.com>
2021-03-14 23:59:30 +0800

    SPI driver, gpio driver skeleton, added newline.

--- src/drivers/user_spi_driver.c
+++ src/drivers/user_spi_driver.c
@@ -3,6 +3,9 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
 
 #include "drivers/user_config_driver.h"
 #include "utils/user_log_util.h"
@@ -31,5 +34,17 @@ user_spi_ret_t user_spi_driver_deinit(user_spi_driver_t *spi) {
 
 user_spi_ret_t user_spi_driver_xfer(user_spi_driver_t *spi, uint8_t *tx_buf,
                                     uint8_t rx_buf, uint32_t len) {
-    //
+    struct spi_ioc_transfer txn = {
+        .tx_buf = (unsigned long)tx_buf,
+        .len = len,
+        .cs_change = 0,
+        .speed_hz = 16000000,
+        .bits_per_word = 8
+    };
+    if(ioctl(spi->spidev_fd, SPI_IOC_MESSAGE(1), &txn) < 0) {
+        USER_LOG(USER_LOG_ERROR, "SPI transaction error.");
+        return USER_SPI_ERROR;
+    }
+
+    return USER_SPI_OK;
 }
